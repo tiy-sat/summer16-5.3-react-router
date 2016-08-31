4053d926f2b8b5428511756642558079f9a3dd54
Jason Vanderslice <jlvanderslice@gmail.com>
2016-08-31 11:27:40 -0500

    Example of App being a parent nested route

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
+import App from './lib/App'
 import Feed from './lib/Feed'
 import Profile from './lib/Profile'
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ Feed } />
-    <Route path="/profile/:username" component={ Profile } />
+    <Route path="/" component={ App }>
+      <Route path="/feed" component={ Feed } />
+      <Route path="/profile/:username" component={ Profile } />
+    </Route>
   </Router>
 ), document.getElementById('app'))
